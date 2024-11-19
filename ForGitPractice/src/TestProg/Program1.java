83b9661f6a7d6ceb4c2160f2dd661632fd4093da
admin <admin@DESKTOP-KGJR2K8>
2024-11-19 12:21:43 +0530

    Added Mul print

--- ForGitPractice/src/TestProg/Program1.java
+++ ForGitPractice/src/TestProg/Program1.java
@@ -6,10 +6,10 @@ public class Program1 {
 
 		int a = 10;
 		int b = 20;
-		int c = b + a;
-		//System.err.println("This is my 1st Program with addition :" + c);
+		int c = b * a;
 		//System.out.println("This is my 1st Program with sub :" + c);
-		System.out.println("This is my 1st Program with add :" + c);
+		//System.out.println("This is my 1st Program with add :" + c);
+		System.out.println("This is mul :" + c);
 
 	}
 
