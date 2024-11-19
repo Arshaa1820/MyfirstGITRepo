c2ea4b36ce425334d5f6172dea6322b81b0c8ed2
admin <admin@DESKTOP-KGJR2K8>
2024-11-19 12:16:44 +0530

    Program1 class added

---
+++ ForGitPractice/src/TestProg/Program1.java
@@ -0,0 +1,15 @@
+package TestProg;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = b - a;
+		//System.err.println("This is my 1st Program with addition :" + c);
+		System.out.println("This is my 1st Program with sub :" + c);
+
+	}
+
+}
